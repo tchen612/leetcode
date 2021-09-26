0a0e270e82cbb69dc2758bd67f647f096e62b038
AI-IS-COOL <thomasc3@student.unimelb.edu.au>
2021-09-26 16:29:54 +1000

    Add coin change solution

---
+++ coin-change.py
@@ -0,0 +1,13 @@
+# Time Complexity = O(an)
+# Space Complexity = O(a)
+# a = amount, n = len(counts)
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] + [float('inf') for x in range(amount)]
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i >= coin:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+        if dp[-1] == float('inf'):
+            return -1
+        return dp[-1]
