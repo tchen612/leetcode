ecce7fd1843a21696fab94faa67336f429a1269d
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 <tchen612@gmail.com>
2021-12-18 20:52:03 +1100

    Add valid parentheses solution

---
+++ amazon/valid-parentheses.py
@@ -0,0 +1,17 @@
+# Time Complexity = O(n)
+# Space Complexity = O(n)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(",
+                   "}": "{",
+                   "]": "["}
+        for char in s:
+            if char in mapping:
+                top = stack.pop() if stack else '#'
+                if mapping[char] != top:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
+        
