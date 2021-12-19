b4a75675c223c2d84e8a9c5bd8cd3f309cdcb182
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 <tchen612@gmail.com>
2021-12-19 22:25:59 +1100

    Add most common word solution

---
+++ amazon/most-common-word.py
@@ -0,0 +1,24 @@
+# Time Complexity = O(n+m)
+# Space Complexity = O(n+m)
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        banned_words = set(banned)
+        ans = ""
+        max_count = 0
+        word_count = defaultdict(int)
+        word_buffer = []
+
+        for i, char in enumerate(paragraph):
+            if char.isalnum():
+                word_buffer.append(char.lower())
+                if i != len(paragraph)-1:
+                    continue
+            if len(word_buffer) > 0:
+                word = "".join(word_buffer)
+                if word not in banned_words:
+                    word_count[word] +=1
+                    if word_count[word] > max_count:
+                        max_count = word_count[word]
+                        ans = word
+                word_buffer = []
+        return ans
